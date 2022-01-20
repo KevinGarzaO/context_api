e3e65b690af839646221fd2dd5f7d779e64217a2
Kevin Garza <kevin_4569@msn.com>
2022-01-19 19:43:31 -0600

    Creando Contexto global

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {ProovedorTema} from './contexts/contextoTema'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProovedorTema>
+      <App />
+    </ProovedorTema>
   </React.StrictMode>,
   document.getElementById('root')
 );
